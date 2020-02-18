b24ec2214eb02ee2e5942740a6e882371495842f
YmiR1710 <volianskyi@ucu.edu.ua>
2020-02-18 13:03:56 +0200

    Bot added

---
+++ ai.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import os
+import neat
+from game import run_game
+import pickle
+
+
+def eval_genomes(genomes, config):
+    for genome_id, genome in genomes:
+        genome.fitness = 0
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        genome.fitness = run_game(net, 3, gui=True)
+
+
+def run(config_file):
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         config_file)
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    p.add_reporter(neat.StatisticsReporter())
+    p.add_reporter(neat.Checkpointer(5))
+    winner = p.run(eval_genomes, 25)
+    print('\nBest genome:\n{!s}'.format(winner))
+    with open('winner_genome', 'wb') as f:
+        pickle.dump(winner, f)
+    winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
+
+
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config-feedforward')
+    run(config_path)
--- game.py
+++ game.py
@@ -2,6 +2,8 @@ import random
 import curses
 import copy
 from sys import argv
+import numpy as np
+import math
 
 
 sc = curses.initscr()
@@ -9,6 +11,14 @@ h, w = sc.getmaxyx()
 
 def fitness_func(steps, apples):
 	return steps + ((2 ** apples) + ((apples ** 2.1) * 500)) - ((apples ** 1.2) * ((steps * 0.25) ** 1.3))
+
+def angle_with_apple(snake_position, apple_position):
+	snake_direction = np.array(snake_position[0]) - np.array(snake_position[1])
+	apple_direction = np.array(apple_position) - np.array(snake_position[0])
+	snake_direction = snake_direction / np.linalg.norm(snake_direction)
+	apple_direction = apple_direction / np.linalg.norm(apple_direction)
+	return math.atan2(snake_direction[0] * apple_direction[1] - snake_direction[1] * apple_direction[0], 
+		snake_direction[0] * apple_direction[0] + snake_direction[1] * apple_direction[1]) / math.pi
 
 def collision_with_apple(score):
 	apple_position = [random.randint(1,h-2),random.randint(1,w-2)]
@@ -181,28 +191,38 @@ def run_game(network, iterations, gui=True):
 		if gui:
 			key = curses.KEY_RIGHT
 		steps = 0
+		steps_without_apple = 0
 		while True:
 			win.border(0)
-			win.timeout(100)
+			win.timeout(20)
 			if gui:
 				next_key = win.getch()
 				if next_key == -1:
 					key = key
 				else:
 					key = next_key
-			actions = [move_front, move_left, move_right]
-			action = random.choice(actions)(snake_head, prev_direction)
-			direction = action[1]
-			snake_head = action[0]
+			actions = [move_right, move_front, move_left]
+			action = network.activate((0 if check_right(snake_head, snake_position, prev_direction) else 1,
+				0 if check_front(snake_head, snake_position, prev_direction) else 1,
+				0 if check_left(snake_head, snake_position, prev_direction) else 1, 
+				angle_with_apple(snake_position, apple_position)))
+			action = actions[np.argmax(np.array(action))]
+			res = action(snake_head, prev_direction)
+			direction = res[1]
+			snake_head = res[0]
 			prev_direction = direction
 			if snake_head == apple_position:
 				apple_position, score = collision_with_apple(score)
 				snake_position.insert(0, list(snake_head))
 				win.addch(apple_position[0], apple_position[1], curses.ACS_DIAMOND)
+				steps_without_apple = 0
 			else:
 				snake_position.insert(0, list(snake_head))
 				last = snake_position.pop()
 				win.addch(last[0], last[1], ' ')
+				steps_without_apple += 1
+			if steps_without_apple >= 300:
+				break 
 			win.addch(snake_position[0][0], snake_position[0][1], 'O')
 			if collision_with_boundaries(snake_head) == 1 or collision_with_self(snake_position) == 1:
 				break
